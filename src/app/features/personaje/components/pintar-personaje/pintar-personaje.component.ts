27ed2dea88189b8e51672c337e4ee118445c863f
victorernan <git config --global user.email>
2021-07-28 20:36:34 +0200

    add functionality to pintar-personaje component

--- src/app/features/personaje/components/pintar-personaje/pintar-personaje.component.ts
+++ src/app/features/personaje/components/pintar-personaje/pintar-personaje.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pintar-personaje',
+  selector: 'app-pintar',
   templateUrl: './pintar-personaje.component.html',
   styleUrls: ['./pintar-personaje.component.css']
 })
 export class PintarPersonajeComponent implements OnInit {
 
   constructor() { }
+
+  @Input() personajes: IPersonaje[];
 
   ngOnInit(): void {
   }
