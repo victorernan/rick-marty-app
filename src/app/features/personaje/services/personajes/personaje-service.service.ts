446b8e73364206978da7133f91d5f21f5c4c51e2
victorernan <git config --global user.email>
2021-07-28 19:24:59 +0200

    create a single reusable url to make HTTPs request

--- src/app/features/personaje/services/personajes/personaje-service.service.ts
+++ src/app/features/personaje/services/personajes/personaje-service.service.ts
@@ -2,45 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { pluck, tap, toArray, catchError } from 'rxjs/operators';
 import { Observable, pipe, throwError } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class PersonajeServiceService {
 
-  baseURL: string = "https://rickandmortyapi.com/api/character";
+  url : string = "https://rickandmortyapi.com/api";
 
   constructor(private httpservice: HttpClient) { }
 
   getAllCharacters():Observable<IPersonaje[]> {
-    return this.httpservice.get<any>(this.baseURL).pipe(pluck('results'));
+    return this.httpservice.get<any>(`${this.url}/character`).pipe(pluck('results'));
   }
 
   getCharacter(char: string):Observable<IPersonaje>{
-    const urlChar = `https://rickandmortyapi.com/api/character/${char}`;
-    return this.httpservice.get<any>(urlChar);
-    /*return this.httpservice.get<any>(`https://rickandmortyapi.com/api/character/?name=${char}`);*/
+    return this.httpservice.get<any>(`${this.url}/character/${char}`);
   }
 
   getEpisode(id: number):Observable<IEpisode>{
-    const urlChar = `https://rickandmortyapi.com/api/episode/${id}`
-    return this.httpservice.get<any>(urlChar);
+    return this.httpservice.get<any>(`${this.url}/episode/${id}`);
   }
 
   searchCharacters(nombre: string):Observable<IPersonaje>{
-    const urlChar = `https://rickandmortyapi.com/api/character/?name=${nombre}`;
-    return this.httpservice.get<any>(urlChar).
+    return this.httpservice.get<any>(`${this.url}/character/?name=${nombre}`).
           pipe(
             pluck('results'),
             catchError(err => this.handleError(err))
           );
     }
-
     handleError(error: HttpErrorResponse){
       console.log(error);
       console.warn("error ", error);
-      return throwError("Error victor");
+      return throwError("Error");
     }
-
-
 }
